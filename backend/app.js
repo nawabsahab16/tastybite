5da9daf68709be707cc7a63942126eaa404227af
nawabsahab16 <Nawab162004@gmail.com>
2024-01-30 03:27:13 +0530

    fix : nodemon issue affecting the server

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,15 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config({path: "./config/config.env"});
 
+app.use(
+    cors ({
+        origin:[process.env.FRONTEND_URL],
+        method: ["POST"],
+        credentials : true,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
 export default app;
